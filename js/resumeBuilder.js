37e5855800145b37a118ebd090a40d3e52ce06a5
yaochen1224 <yao.chen1224@foxmail.com>
2017-02-28 23:08:44 -0500

    new commitment

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,8 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ var formattedName = HTMLheaderName.replace("%data%","Yao Chen");
+ $("#header").append(formattedName);
+
+var formattedRole = HTMLheaderRole.replace("%data%","Web-developer, Instructional Designer")
+$("#header").append(formattedRole);
