ecfb14f497ffbf6047f103eeb70217fc1638dc8a
yaochen1224 <yao.chen1224@foxmail.com>
2017-03-03 15:22:01 -0500

    new commitment
    layout, content, images

--- js/helper.js
+++ js/helper.js
@@ -53,7 +53,7 @@ var HTMLonlineClasses = '<h3>Online Classes</h3>';
 var HTMLonlineTitle = '<a href="#">%data%';
 var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
-var HTMLonlineURL = '<br><a href="#">%data%</a>';
+var HTMLonlineURL = '<br><a href="#" class="url">%data%</a>';
 
 var internationalizeButton = '<button>Internationalize</button>';
 var googleMap = '<div id="map"></div>';
@@ -127,7 +127,7 @@ function initializeMap() {
     var locations = [];
 
     // adds the single location property from bio to the locations array
-    locations.push(bio.contacts.location);
+    locations.push(bio.contactInfo.location);
 
     // iterates through school locations and appends each location to
     // the locations array. Note that forEach is used for array iteration
@@ -141,7 +141,7 @@ function initializeMap() {
     // the locations array. Note that forEach is used for array iteration
     // as described in the Udacity FEND Style Guide:
     // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
-    work.jobs.forEach(function(job){
+    work.job.forEach(function(job){
       locations.push(job.location);
     });
 
@@ -239,11 +239,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+ map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,8 +1,204 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- var formattedName = HTMLheaderName.replace("%data%","Yao Chen");
- $("#header").append(formattedName);
+var bio = {
+  name: "Yao Chen",
+  role: "Web-developer, Instructional Designer",
+  contactInfo:{ mobile: "9178157626",
+                email: "yc3185@tc.columbia.edu",
+                github: "https://github.com/yaochen1224",
+                location: "1230 Amsterdam Ave"},
+  welcomeMessage: "Welcome to my online resume!",
+  skills: ["Microsoft Office", "Java", "Python", "JavaScript","Html","C++","R"],
+  bioPic: "images/photo.jpeg"
+};
+function inName(name) {
+  var nameArray = name.trim().split(" ");
+  var firstName = nameArray[0].charAt(0).toUpperCase() + nameArray[0].slice(1).toLowerCase();
+  var lastName = nameArray[1].toUpperCase();
+  return firstName + " " + lastName;
+}
+$("#main").append(internationalizeButton);
 
-var formattedRole = HTMLheaderRole.replace("%data%","Web-developer, Instructional Designer")
-$("#header").append(formattedRole);
+bio.displayBio = function(){
+  var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
+  $("#header").prepend(formattedRole);
+
+  var formattedName = HTMLheaderName.replace("%data%",bio.name);
+  $("#header").prepend(formattedName);
+
+  var formattedImage = HTMLbioPic.replace("%data%",bio.bioPic);
+  $("#header").append(formattedImage);
+
+  var formattedMobile = HTMLmobile.replace("%data%", bio.contactInfo.mobile);
+  var formattedEmail = HTMLemail.replace("%data%", bio.contactInfo.email);
+  var formattedGithub = HTMLgithub.replace("%data%", bio.contactInfo.github);
+  var formattedLocation = HTMLlocation.replace("%data%", bio.contactInfo.location);
+  $("#topContacts").append(formattedMobile);
+  $("#topContacts").append(formattedEmail);
+  $("#topContacts").append(formattedGithub);
+  $("#topContacts").append(formattedLocation);
+
+
+  if(bio.skills.length > 0){
+    $("#header").append(HTMLskillsStart);
+    var formattedSkill = "";
+
+    for(var i=0; i < bio.skills.length; i++ ){
+      formattedSkill = HTMLskills.replace("%data%",bio.skills[i]);
+      $("#skills").append(formattedSkill);
+    }
+  }
+}
+
+bio.displayBio();
+
+
+
+
+var work = {
+  "job" : [
+  {
+  "title" : "Data Analyst",
+  "employer" : "Millward Brown",
+  "years" : "In Progress",
+  "location" : "Beijing, China",
+  "description" : "Dealt with data"
+  },
+  {
+  "title" : "Data Analyst",
+  "employer" : "Millward Brown",
+  "years" : "0.4",
+  "location" : "Beijing, China",
+  "description" : "Dealt with data"
+  }]
+}
+
+
+work.displayWork = function(){
+  $("#workExperience").append(HTMLworkStart);
+  for(var i=0; i < work.job.length; i++){
+    var formattedEmployer = HTMLworkEmployer.replace("%data%",work.job[i].employer);
+    var formattedTitle = HTMLworkTitle.replace("%data%",work.job[i].title);
+    $(".work-entry").append(formattedEmployer+formattedTitle);
+    var formattedLocation = HTMLworkLocation.replace("%data%",work.job[i].location);
+    $(".work-entry").append(formattedLocation);
+    var formattedDates = HTMLworkDates.replace("%data%",work.job[i].years);
+    $(".work-entry").append(formattedDates);
+    var formattedDescription = HTMLworkDescription.replace("%data%",work.job[i].description);
+    $(".work-entry").append(formattedDescription);
+  }
+}
+
+work.displayWork();
+
+var education = {
+  "schools":[
+    {
+    "schoolName":"Teachers College, Columbia University",
+    "location": "New York, NY, US",
+    "degree": "Master of Art",
+    "majors" : ["Instructional Technology and Media"],
+    "dates" : "2016 - 2018",
+  },
+    {
+    "schoolName":"University of International Business and Economics",
+    "location": "Beijing, CHINA",
+    "degree": "Bachelor of Art",
+    "majors" : ["Teaching Chinese as a Second Language","Finance"],
+    "dates" : "2012 - 2016",
+    }
+  ],
+
+  "onlineCourses":[
+    {
+      "title": "Front-end Developer Nanodgree",
+      "school":"Udacity",
+      "date":"2017",
+      "url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
+    },
+    {
+      "title": "Python for Informatics",
+      "school": "Coursera",
+      "date": "2016",
+      "url": "https://www.coursera.org/specializations/python"
+    }
+  ]
+}
+
+education.displayEducation = function() {
+  for (school in education.schools) {
+    $("#education").append(HTMLschoolStart);
+    var formattedSchoolLocation = HTMLschoolLocation.replace("%data%",education.schools[school].location);
+    var formattedSchoolName = HTMLschoolName.replace("%data%",education.schools[school].schoolName);
+    var formattedSchoolDgree = HTMLschoolDegree.replace("%data%",education.schools[school].degree);
+    var formattedSchoolMajors = HTMLschoolMajor.replace("%data%",education.schools[school].majors);
+    var formattedSchoolDates = HTMLschoolDates.replace("%data%",education.schools[school].dates);
+    $(".education-entry:last").append(formattedSchoolName+formattedSchoolDgree);
+    // $(".education-entry:last").append(formattedSchoolDgree);
+    $(".education-entry:last").append(formattedSchoolDates);
+    $(".education-entry:last").append(formattedSchoolLocation);
+    $(".education-entry:last").append(formattedSchoolMajors);
+  }
+
+  if(education.onlineCourses.length>0){
+    $("#education").append(HTMLonlineClasses);
+    for (ecourse in education.onlineCourses){
+      var onlineTitle = HTMLonlineTitle.replace("%data%",education.onlineCourses[ecourse].title);
+      var onlineSchool = HTMLonlineSchool.replace("%data%",education.onlineCourses[ecourse].school);
+      var onlineDate = HTMLonlineDates.replace("%data%",education.onlineCourses[ecourse].date);
+      var onlineURL = HTMLonlineURL.replace("%data%",education.onlineCourses[ecourse].url);
+      $("#education").append('<div class = "onlineCourses"></div>');
+      $(".onlineCourses:last").append(onlineTitle+onlineSchool);
+      $(".onlineCourses:last").append(onlineDate);
+      $(".onlineCourses:last").append(onlineURL);
+    }
+  }
+}
+education.displayEducation();
+
+var projects = {
+  "project": [
+    {"title":"STD Crisis",
+    "dates":"2016.11-2017.3",
+    "description":"STD Crisis is an educational board game that teaches about what damages could STDs cause, and how to cure/prevent them.",
+    "images":["images/organs.png", "images/STD.png","images/cureMethods.png"]
+    },
+    {"title":"AIESEC Global Community Development Programme",
+    "dates":"2014.7–2014.8",
+    "description":"Gave daily lessons to local students and taught them to speak simple Chinese words.<br/>Sold handwriting postcards and collected donations to build a reading room for local students.<br/>Improved the lightening system in schools in slums.<br/>Designed and handed out brochures on the prevention of AIDS.",
+    "images":["images/slum1.png", "images/slum2.png","images/slum3.png"]
+    }
+  ]
+}
+
+projects.displayProject = function() {
+  for (project in projects.project) {
+    $("#projects").append(HTMLprojectStart);
+    var formattedTitle = HTMLprojectTitle.replace("%data%",projects.project[project].title);
+    $(".project-entry:last").append(formattedTitle);
+    var formattedDates = HTMLprojectDates.replace("%data%",projects.project[project].dates);
+    $(".project-entry:last").append(formattedDates);
+    var formattedDescription = HTMLprojectDescription.replace("%data%",projects.project[project].description);
+    $(".project-entry:last").append(formattedDescription);
+    $(".project-entry:last").append('<div class="project-img"></div>')
+    if(projects.project[project].images.length>0){
+      for (image in projects.project[project].images) {
+        var formattedImage = HTMLprojectImage.replace("%data%",projects.project[project].images[image]);
+        $(".project-img:last").append(formattedImage);
+      }
+    }
+
+  }
+}
+projects.displayProject();
+
+$(document).click(function(loc){
+  console.log(1);
+  var x = loc.pageX;
+  var y = loc.pageY;
+  console.log(x + " " + y);
+  logClicks(x,y);
+});
+
+$("#mapDiv").append(googleMap);
